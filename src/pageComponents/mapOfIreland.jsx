14e985bdf647314079b61ee25a2e5dfa8741a5fc
swordslinger <seanskelton40@gmail.com>
2021-03-19 21:39:03 +0000

    changed data source for gejson and still working on displaying api data in markers

--- src/pageComponents/mapOfIreland.jsx
+++ src/pageComponents/mapOfIreland.jsx
@@ -1,9 +1,10 @@
 // imports
 import React from 'react';
-import { MapContainer, GeoJSON } from "react-leaflet"
-import counties from '../data/counties.json'
+import { MapContainer, GeoJSON, Marker, Popup, TileLayer  } from "react-leaflet"
+import towns from '../data/towns.json'
 import "leaflet-css"
 import "./mapOfIreland.css"
+
 
 
 
@@ -31,7 +32,8 @@ export class MapOfIreland extends React.Component {
 
         //testing to ensure coordinates are accesible 
            // console.log(data.features[0].geometry.x)
-       
+
+        //coordinates and cases for one area
         this.setState({ coordinates: data.features[0].geometry, cases: data.features[0].attributes.ConfirmedCovidCases})
         console.log(this.state)
 
@@ -40,21 +42,29 @@ export class MapOfIreland extends React.Component {
 
     //renders elements on screen
     render() {
+        const position = [54, -8]
         return (
             <div>
             {/*Manul test to let user know if they are on the map of ireland page*/}
                 <h1 style={{ color: "red", textAlign: "center" }}>This is a Test for Map of Ireland link</h1>
                 <h1 style = {{ textAlign: "center"}}>Map of Ireland</h1>
                 {/*creates an instance of a leaflet map*/}
-                <MapContainer style = {{height:"100vh"}} zoom = {2} center={[20, 20]}>
-                        {/*draws on the leaflet map with the json data from counties.json */}
-                        <GeoJSON data = {counties.features} />
+            
+
+                <MapContainer style = {{height:"90vh"}} zoom = {3} center={[0, 0]}>
+                    {/*draws on the leaflet map with the json data from counties.json */}
+                    <GeoJSON data = {towns.features} />
+                    
+                    {towns.map(town =>
+                        <Marker>
+                            key = {town.FID}
+                            
+                        </Marker>
+                    )}
                 </MapContainer>
-
-                
-
-                
-            </div>
-        );
+                </div>
+      
+            
+        )
     }
 }
