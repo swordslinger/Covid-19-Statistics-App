26ed1ff2edbe66a6fdcebb3438216419f5771e39
swordslinger <seanskelton40@gmail.com>
2021-04-23 19:40:46 +0100

    added aysnyhronus call

--- src/pageComponents/searchAPI.js
+++ src/pageComponents/searchAPI.js
@@ -11,41 +11,49 @@ export class SearchAPI extends React.Component {
         }
     }
 
-    componentDidMount(){
-        axios.get('https://services1.arcgis.com/eNO7HHeQ3rUcBllm/arcgis/rest/services/CovidStatisticsProfileHPSCIrelandOpenData/FeatureServer/0/query?where=FID%20%3E%3D%20405%20AND%20FID%20%3C%3D%20405&outFields=CovidCasesConfirmed,FID,ConfirmedCovidCases,ConfirmedCovidDeaths&outSR=4326&f=json')//asynchronous by using a promise //now gets data from new api
-        .then(
-            (response) => {
-            this.setState({ results: response.data })//data coming back as from the response of the web server
-            }//response method
-        )
-        .catch((error) => {
-        console.log(error)//logs error to console
-        });//if things dont work catch method
-    }
-
-    searchAPI = (e) => {
+    enterFID = (e) => {
+        e.preventDefault();
         this.setState({
             fid: e.target.value
         })
- 
+        
+        
+    }
+
+    searchAPI = (e) => {
+        e.preventDefault();
+        axios.get("https://services1.arcgis.com/eNO7HHeQ3rUcBllm/arcgis/rest/services/CovidStatisticsProfileHPSCIrelandOpenData/FeatureServer/0/query?where=FID%20%3E%3D%20405%20AND%20FID%20%3C%3D%20405&outFields=CovidCasesConfirmed,FID,ConfirmedCovidCases,ConfirmedCovidDeaths&outSR=4326&f=json")
+        .then(res =>{
+            this.state.results = res
+        }.catch(error)
+        {
+
+        })
+
     }
 
     
 
     render (){
+        console.log(this.state.fid)
         return(
             <div className='App'>
-
+                <form onSubmit={this.searchAPI}>
                 <div className="form-group">
                     <label>Add Fid: </label>
-                    <h1>{this.state.results.fields.name}</h1>
+                    <h1>{this.state.fid}</h1>
                     <input type='text' className='form-control'  minlength="1" maxlength="3"
                         value={this.state.fid}//requires user to add @ and . com to input otherwise a warning popup will appear
-                        onChange={this.searchAPI}>
+                        onChange={this.enterFID}>
                     </input>
                 </div>
+                </form>
         </div>
+
+        
         )
     }
 
 }
+
+//'https://services1.arcgis.com/eNO7HHeQ3rUcBllm/arcgis/rest/services/CovidStatisticsProfileHPSCIrelandOpenData/FeatureServer/0/query?where=FID%20%3E%3D%20405%20AND%20FID%20%3C%3D%20405&outFields=CovidCasesConfirmed,FID,ConfirmedCovidCases,ConfirmedCovidDeaths&outSR=4326&f=json')//asynchronous by using a promise //now gets data from new api
