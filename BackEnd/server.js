9fe35a69bdbfed17d4e70135270ecc9e56736ac4
FionnBrowne <49272530+FionnBrowne@users.noreply.github.com>
2021-04-23 21:58:40 +0100

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -89,5 +89,5 @@ app.get('*', (req, res) => {//will give all roots and send file back from index.
     res.sendFile(path.join(__dirname + '/../build/index.html'));//sending a file and joining two paths
 })
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App is listening at http://localhost:${port}`)
 })
